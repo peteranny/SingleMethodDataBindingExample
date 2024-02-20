1949a2bfd0e2efdb73e5ac2cd12f4353ebfd9c65
peter.shih <peter.shih@cardinalblue.com>
2024-02-20 10:12:42 +0800

    Initial Commit

---
+++ SimpleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleProject
+//
+//  Created by Peter Shih on 2024/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleProjectUITests/SimpleProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleProjectUITestsLaunchTests.swift
+//  SimpleProjectUITests
+//
+//  Created by Peter Shih on 2024/2/20.
+//
+
+import XCTest
+
+final class SimpleProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
